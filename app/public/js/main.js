d1066454bcb18cb4ae88ce21c50876007c77b7d3
Ragland <jakearagland@gmail.com>
2018-06-30 11:38:20 -0500

    fixed the signup page and added more script for the profile page.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -27,6 +27,60 @@ console.log(password);
 /// Login for new user page////////////////
 
 function validateForm2() {
+
+    var email =  document.getElementById('email').value;
+    if (email == "") {
+        document.getElementById('status').innerText = "Email cannot be empty";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(email)){
+            document.getElementById('email').innerText = "Email format invalid";
+            return false;
+        }
+    }
+    var password =  document.getElementById('password').value;
+    if (password == "") {
+        document.getElementById('status').innerText = "Subject cannot be empty";
+        return false;
+    }
+    var first =  document.getElementById('first').value;
+    if (first == "") {
+        document.getElementById('status').innerText = "Subject cannot be empty";
+        return false;
+    }
+    var last =  document.getElementById('last').value;
+    if (last == "") {
+        document.getElementById('status').innerText = "Subject cannot be empty";
+        return false;
+    }
+    var age =  document.getElementById('inputAge').value;
+    if (age == "") {
+        document.getElementById('status').innerText = "Subject cannot be empty";
+        return false;
+    }
+    document.getElementById('status').innerText = "Sending...";
+    document.getElementById('submit2').submit();
+
+}
+
+//img uplaod
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = function (e) {
+        $('#blah')
+          .attr('src', e.target.result);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  /// Update protfolo page.
+  function validateForm3() {
     var picture =  document.getElementById('file').value;
     if (picture == "") {
         document.getElementById('status').innerText = "Subject cannot be empty";
@@ -75,20 +129,4 @@ function validateForm2() {
     }
     document.getElementById('status').innerText = "Sending...";
     document.getElementById('submit2').submit();
-
-}
-
-//img uplaod
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-  
-      reader.onload = function (e) {
-        $('#blah')
-          .attr('src', e.target.result);
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-    }
   }
